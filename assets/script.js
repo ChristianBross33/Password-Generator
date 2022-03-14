7b0a02b0584670ed8b045523f579abf50e9429e0
cbross <cbross021303@gmail.com>
2022-03-14 18:04:24 -0400

    set variable changed, and user questions added to java

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,23 @@ var upperC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numC = '0123456789';
 var specialC = '~!@#$%^&*()_+=';
 
+// added function to ask user which options
+function questionPrompts(){
+  var isValid = false;
+  do {
+    var passwordLength = prompt("How many characters would you like your password to be? Your password must be between 8-128 characters long.");
+    var numbersChoice = confirm("A good password needs some numbers. Want to add some?");
+    var lowercaseChoice = confirm("How about some lowercase letters?");
+    var uppercaseChoice = confirm("What about some uppercase letters?");
+    var specialChoice = confirm("You have a good password, but great one has special characters. Want to add that too?");
+    var responses = {
+      length: passwordLength,
+      numbersChoice: numbersChoice,
+      lowercaseChoice: lowercaseChoice,
+      uppercaseChoice: uppercaseChoice,
+      specialChoice: specialChoice
+    }
+    
 function generatePassword() {
   console.log("Hey! You clicked the button!")
 
