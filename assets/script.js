4c658e3e583ab607edab4028d1588c984ae20fa4
cbross <cbross021303@gmail.com>
2022-03-16 16:36:05 -0400

    Fixxed undefined message

--- assets/script.js
+++ assets/script.js
@@ -5,71 +5,63 @@ var generateBtn = document.querySelector("#generate");
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
-];
+var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
 
 // added function to ask user which options
 function questionPrompts(){
   var isValid = false;
   do {
-    var passwordLength = prompt("How many characters would you like your password to be? Your password must be between 8-128 characters long.");
-    var numbersChoice = confirm("A good password needs numbers. Do you want to add some?");
-    var lowercaseChoice = confirm("Do you want your password to include lower case letters?");
-    var uppercaseChoice = confirm("Do you want your password to include upper case letters?");
-    var specialChoice = confirm("You have a good password, but good ones have special characters. Want to add that too?");
+    var length = prompt("How many characters would you like your password to be? Your password must be between 8-128 characters long.");
+    var numbersChoice2 = confirm("A good password needs numbers. Do you want to add some?");
+    var lowercaseChoice3 = confirm("Do you want your password to include lower case letters?");
+    var uppercaseChoice4 = confirm("Do you want your password to include upper case letters?");
+    var specialChoice5 = confirm("You have a good password, but good ones have special characters. Want to add that too?");
     var responses = {
-      length: passwordLength,
-      numbersChoice: numbersChoice,
-      lowercaseChoice: lowercaseChoice,
-      uppercaseChoice: uppercaseChoice,
-      specialChoice: specialChoice
+      length: length,
+      numbersChoice2: numbersChoice2,
+      lowercaseChoice3: lowercaseChoice3,
+      uppercaseChoice4: uppercaseChoice4,
+      specialChoice5: specialChoice5
     } 
+
     if((length < 8)||(length > 128))
     alert("Choose number between 8 and 128");
-    else if((!askNumbers)&&(!askLowerCase)&&(!askUpperCase)&&(!askSpecial))
+    else if((!numbersChoice2)&&(!lowercaseChoice3)&&(!uppercaseChoice4)&&(!specialChoice5))
     alert("Must choose at least one type.");
     else
     isValid = true;
 
   } while(!isValid);
   return responses;
-}
+};
 
 // Joining everything together and generating final result for password.
 function generatePassword() {
-  var passwordOptions = questions();
+  var passwordOptions = questionPrompts();
   var possibleCombo = [];
   var finalPassword = "";
 
-
-
-  if (passwordOptions.askNumbers) {
+  if (passwordOptions.numbersChoice2) {
     for (var i of numbers)
       possibleCombo.push(i);
   }
-  if (passwordOptions.askLowerCase) {
+  if (passwordOptions.lowercaseChoice3) {
     for (var i of lowerCase)
       possibleCombo.push(i);
   }
-  if (passwordOptions.askUpperCase) {
+  if (passwordOptions.uppercaseChoice4) {
     for (var i of upperCase)
       possibleCombo.push(i);
   }
-  if (passwordOptions.askSpecial) {
+  if (passwordOptions.specialChoice5) {
     for (var i of special)
       possibleCombo.push(i);
   }
 
-
-  console.log(possibleCombo);
-
-
   for (var i = 0; i < passwordOptions.length; i++) {
     finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
-    
   }
-  console.log(finalPassword);
 
   return finalPassword;
 }
